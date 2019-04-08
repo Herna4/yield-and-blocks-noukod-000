bc2e7373ea80e80a410cd7ddbaefa05ec1bd703c
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-08 19:32:35 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,7 +2,7 @@ def hello_t(array)
    i = 0
  
   while i < array.length
-    yield(array[i]) #calls the block once for each element in the passed-in array 
+    yield(array[i]){} #calls the block once for each element in the passed-in array 
     i = i + 1
   end
    array #return the original array 
